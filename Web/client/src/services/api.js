545cd56d9c22dd88fa446f4b1adc934c892c1451
Kalpesh Paliwal <142911780+Kalpeshcoder18@users.noreply.github.com>
2026-02-17 21:42:50 +0530

    api

--- Web/client/src/services/api.js
+++ Web/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://topsis-3s7c.vercel.app/api",
+  baseURL: "https://topsis-102317087-25yp.vercel.app/api",
 });
 
 export default api;
